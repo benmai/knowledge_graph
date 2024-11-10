802bc688dae73c0f5f1112994723c37b093c2913
Ben Isaacs <7683112+benmai@users.noreply.github.com>
2024-11-10 15:19:36 -0800

    Don't evalute priv_dir at compile time

--- lib/knowledge_graph/knowledge_graph.ex
+++ lib/knowledge_graph/knowledge_graph.ex
@@ -7,15 +7,12 @@ defmodule KnowledgeGraph.Link do
 end
 
 defmodule KnowledgeGraph.KnowledgeGraph do
-  # Current working directory concatenated with the path to the nodes file
-  # @nodes_path Path.join([:code.priv_dir(:knowledge_graph), "repo", "nodes.json"])
-
   def list_nodes() do
-    # @nodes_path
-    # |> File.read!()
-    # |> Poison.decode!(as: [%KnowledgeGraph.Node{links: [%KnowledgeGraph.Link{}]}])
-
-    []
+    # It's not possible to declare this at the module level since that's evaluated
+    # at compile-time.
+    Path.join([:code.priv_dir(:knowledge_graph), "repo", "nodes.json"])
+    |> File.read!()
+    |> Poison.decode!(as: [%KnowledgeGraph.Node{links: [%KnowledgeGraph.Link{}]}])
   end
 
   def get_node!(id) do
