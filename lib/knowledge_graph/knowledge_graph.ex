cdc8008fca7c606c716d869fd6afed47f77a2429
Ben Isaacs <7683112+benmai@users.noreply.github.com>
2024-11-10 15:07:20 -0800

    Temporarily return empty list of nodes

--- lib/knowledge_graph/knowledge_graph.ex
+++ lib/knowledge_graph/knowledge_graph.ex
@@ -8,12 +8,14 @@ end
 
 defmodule KnowledgeGraph.KnowledgeGraph do
   # Current working directory concatenated with the path to the nodes file
-  @nodes_path Path.join([:code.priv_dir(:knowledge_graph), "repo", "nodes.json"])
+  # @nodes_path Path.join([:code.priv_dir(:knowledge_graph), "repo", "nodes.json"])
 
   def list_nodes() do
-    @nodes_path
-    |> File.read!()
-    |> Poison.decode!(as: [%KnowledgeGraph.Node{links: [%KnowledgeGraph.Link{}]}])
+    # @nodes_path
+    # |> File.read!()
+    # |> Poison.decode!(as: [%KnowledgeGraph.Node{links: [%KnowledgeGraph.Link{}]}])
+
+    []
   end
 
   def get_node!(id) do
